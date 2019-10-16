a6c7f22332494ed3dc1e76e86321f756dd3953eb
iswarya-lab <ishupriya1004@gmail.com>
2019-10-16 17:43:19 +0530

    First commit on ngx

---
+++ src/app/@core/data/employee-details.ts
@@ -0,0 +1,5 @@
+
+export abstract class EmployeeDetailsData {
+    abstract getData(): any[];
+  }
+  
---
+++ src/app/pages/tables/employee-details/employee-details.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+
+import { EmployeeDetailsData } from '../../../@core/data/employee-details';
+
+@Component({
+  selector: 'ngx-employee-details',
+  templateUrl: './employee-details.component.html',
+  styleUrls: ['./employee-details.component.scss'],
+})
+export class EmployeeDetailsComponent {
+
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+      },
+      firstName: {
+        title: 'First Name',
+        type: 'string',
+      },
+      lastName: {
+        title: 'Last Name',
+        type: 'string',
+      },
+      username: {
+        title: 'Username',
+        type: 'string',
+      },
+      email: {
+        title: 'E-mail',
+        type: 'string',
+      },
+      age: {
+        title: 'Age',
+        type: 'number',
+      },
+    },
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(private service: EmployeeDetailsData) {
+    const data = this.service.getData();
+    this.source.load(data);
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
+
+ 
+  
+}
